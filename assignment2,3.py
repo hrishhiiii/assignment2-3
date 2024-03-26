adb2f24eeb6999ee3f9adff5865c7a3a0163141c
hrishhiiii <163642803+hrishhiiii@users.noreply.github.com>
2024-03-26 15:56:02 +0530

    Add files via upload

---
+++ assignment2,3.py
@@ -0,0 +1,60 @@
+# length = 7
+# breadth = 4
+# area = length * breadth
+# perimeter =2 * (length + breadth)
+# print("area of the rectangle=",area)
+
+# list:
+
+# a = [2,4,6,8]
+# b =a[1:3]
+# print(b)
+# a.append(10)
+# print(a)
+# a.insert(3,7)
+# print(a)
+# a.remove(2)
+# print(a)
+# print(a[0])
+# l=len(a)
+# print(l)
+# list1= [1,2,3]
+# list2=[4,5,6]
+# c = list1 + list2
+# print(c)
+# print(list1 * 2)
+
+
+#1
+
+# X = [[12,7],
+#     [4 ,5],
+#     [3 ,8]]
+# result = [[0,0,0],
+#          [0,0,0]]
+# for i in range(len(X)):
+#     for j in range(len(X[0])):
+#         result[j][i]=X[i][j]
+# for r in result:
+#     print(r)
+
+# #4
+# d1 ={1: 'a',2: 'b'}
+# d2 ={2: 'c',4: 'd'}
+# print(d1 | d2)
+
+#6
+# a =[8,10,19]
+# total=sum(a)
+# print("sum of all elements in the given list:",total)
+        
+        
+#10
+# numbers =[2,4,8]
+# doubled_numbers = [num * 2 for num in numbers]
+# print(doubled_numbers)
+
+#11
+# dic=dict.fromkeys(range(5), True)
+# print(dic)
+
